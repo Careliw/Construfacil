f4a260d342d7cb3b0ebb132958d587652eb4ddcb
Wesley Careli <wesleycareli79@gmail.com>
2025-11-20 04:10:46 +0000

    faça a mesma atualização no rodapé para a tela inicial e tela de ajuda

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,8 @@ export function Footer() {
   return (
     <footer className="py-5 mt-12 border-t">
       <div className="container mx-auto text-center text-sm text-muted-foreground">
-        <p>Desenvolvido por Wesley Careli</p>
+        <p className="font-semibold">Desenvolvido por Wesley Careli</p>
+        <p className="text-xs">v1.0.0 | Todos os direitos reservados.</p>
       </div>
     </footer>
   );
